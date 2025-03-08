eabf0475f1055b6172e8de6511e5792a4312388d
zheladev <zheladev@gmail.com>
2025-03-08 23:54:43 +0100

    Initial Commit

---
+++ src/Main.cs
@@ -0,0 +1,47 @@
+namespace ADHDGame;
+
+using Godot;
+using Chickensoft.GameTools.Displays;
+
+#if DEBUG
+using System.Reflection;
+using Chickensoft.GoDotTest;
+#endif
+
+// This entry-point file is responsible for determining if we should run tests.
+//
+// If you want to edit your game's main entry-point, please see Game.tscn and
+// Game.cs instead.
+
+public partial class Main : Node2D {
+  public Vector2I DesignResolution => Display.UHD4k;
+#if DEBUG
+  public TestEnvironment Environment = default!;
+#endif
+
+  public override void _Ready() {
+    // Correct any erroneous scaling and guess sensible defaults.
+    GetWindow().LookGood(WindowScaleBehavior.UIFixed, DesignResolution);
+
+#if DEBUG
+    // If this is a debug build, use GoDotTest to examine the
+    // command line arguments and determine if we should run tests.
+    Environment = TestEnvironment.From(OS.GetCmdlineArgs());
+    if (Environment.ShouldRunTests) {
+      CallDeferred("RunTests");
+      return;
+    }
+#endif
+
+    // If we don't need to run tests, we can just switch to the game scene.
+    CallDeferred("RunScene");
+  }
+
+#if DEBUG
+  private void RunTests()
+    => _ = GoTest.RunTests(Assembly.GetExecutingAssembly(), this, Environment);
+#endif
+
+  private void RunScene()
+    => GetTree().ChangeSceneToFile("res://src/Game.tscn");
+}
---
+++ test/src/GameTest.cs
@@ -0,0 +1,31 @@
+namespace ADHDGame;
+
+using System.Threading.Tasks;
+using Godot;
+using Chickensoft.GoDotTest;
+using Chickensoft.GodotTestDriver;
+using Chickensoft.GodotTestDriver.Drivers;
+using Shouldly;
+
+public class GameTest : TestClass {
+  private Game _game = default!;
+  private Fixture _fixture = default!;
+
+  public GameTest(Node testScene) : base(testScene) { }
+
+  [SetupAll]
+  public async Task Setup() {
+    _fixture = new Fixture(TestScene.GetTree());
+    _game = await _fixture.LoadAndAddScene<Game>();
+  }
+
+  [CleanupAll]
+  public void Cleanup() => _fixture.Cleanup();
+
+  [Test]
+  public void TestButtonUpdatesCounter() {
+    var buttonDriver = new ButtonDriver(() => _game.TestButton);
+    buttonDriver.ClickCenter();
+    _game.ButtonPresses.ShouldBe(1);
+  }
+}
